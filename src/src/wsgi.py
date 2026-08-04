2b6d19894c55bb3eebc8891ce8e132f5604e6894
Jerase <nkonoemelda@gmail.com>
2026-08-04 17:55:06 +0100

    fixed wsgi settings import

--- src/src/wsgi.py
+++ src/src/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.src.settings')
 
 application = get_wsgi_application()
 
